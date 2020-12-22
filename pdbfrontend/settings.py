646be5ca6a61371dd1bbf00f66ae901a7537dec1
Farril Zavier Fernaldy <farrilf@gmail.com>
2020-12-22 23:58:44 +0700

    update cors whitelist

--- pdbfrontend/settings.py
+++ pdbfrontend/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://simonik-a7.herokuapp.com"
 ]
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'Access-Control-Allow-Origin',
